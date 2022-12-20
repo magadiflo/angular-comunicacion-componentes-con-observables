c272470f2c74a298cfa3b07a78ee7a2a286f4003
Martín <magadiflo@gmail.com>
2022-12-20 10:57:12 -0500

    Usando directamente el emitter en el html hijo y usando el pipe ASYNC para hacer el unsubscribe() automáticamente

--- src/app/components/hijo/hijo.component.ts
+++ src/app/components/hijo/hijo.component.ts
@@ -13,25 +13,29 @@ export class HijoComponent implements OnInit, OnDestroy {
   mensaje: string = 'Mensaje...!';
   nombreSubscription: Subscription;
 
-  constructor(private dataService: DataService) { }
+  constructor(public dataService: DataService) { }
 
   ngOnInit() {
+    /*
     //* Cuando nosotros disparamos un subscribe, da como producto una subscripción y esa es la que almacenamos en la variable nombreSubscription
-    this.nombreSubscription = this.dataService.nombre$
-      .subscribe((nombre: string) => {
+    *this.nombreSubscription = this.dataService.nombre$
+    *  .subscribe((nombre: string) => {
         //* Veremos que en el console.log este se está repitiendo varias veces porque, si bien es cierto,
         //* este componente HijoComponent, se destruye, pero la subscripción realizada al nombre$ sigue
         //* estando en memoria, deberíamos destruirla en el ngOnDestroy
-        console.log('Hijo: ' + nombre);
-        this.mensaje = nombre;
-      });
+    *    console.log('Hijo: ' + nombre);
+    *    this.mensaje = nombre;
+    *  });
+      */
   }
 
   ngOnDestroy(): void {
+    /*
     //* De esta manera destruimos la subscripción realizada. Ahora cuando veamos el console.log(), veremos
     //* que se muestra una vez el log cada que se entra a este componente.
-    console.log('Hijo: Destruyendo subscribe()');
-    this.nombreSubscription.unsubscribe();
+    * console.log('Hijo: Destruyendo subscribe()');
+    * this.nombreSubscription.unsubscribe();
+    */
   }
 
 }
